5b81fc540a2145b98cf4a1500f81de33f460ab0d
Jianshao <787903+jianshaow@users.noreply.github.com>
2020-10-12 15:03:45 +0800

    update

--- flask/ip-simulatror/app.py
+++ flask/ip-simulatror/app.py
@@ -2,11 +2,19 @@ from flask import Flask, abort, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/pre-get-claims', methods=['POST'])
-def pre_get_claims():
-    if not request.json or 'id' not in request.json or 'info' not in request.json:
+@app.route('/post-get-claims', methods=['POST'])
+def post_get_claims():
+    if not request.json or 'userId' not in request.json:
         abort(400)
-    return jsonify({'result': 'success'})
+    user_id = request.json['userId']
+
+    credit_score = 0
+    if user_id == '1':
+        credit_score = 10
+    if user_id == '2':
+        credit_score = 20
+
+    return jsonify({'credit-score': credit_score})
 
 if __name__ == '__main__':
     app.run(debug=True)

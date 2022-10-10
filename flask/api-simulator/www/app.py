7e6acf5640dc40b992d7f4f16abbbeb1a0df6de6
Jianshao <787903+jianshaow@users.noreply.github.com>
2022-10-10 14:38:20 +0800

    update

--- flask/api-simulator/www/app.py
+++ flask/api-simulator/www/app.py
@@ -1,5 +1,9 @@
-from flask import Flask, request, render_template
+import json
 import logging
+import os
+import sys
+
+from flask import Flask, request
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -9,44 +13,24 @@ his_logger.setLevel(logging.INFO)
 
 app = Flask(__name__)
 
-offset = 0x1000
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.tmpl')
+resp_body_dir = '/www/resp_body'
 
 
-@app.route('/encode', methods=['POST'])
-def encode():
-    string = request.json['text']
-    encoded = []
-    for c in string:
-        n = c
-        order = ord(c)
-        if 0x4e00 <= order <= 0x9fa5:
-            n = chr(order - offset)
-        encoded.append(n)
-        logger.debug('%s(%d) to %s(%d)', c, ord(c), n, ord(n))
-    result = ''.join(encoded)
-    his_logger.info('%s -> %s', string, result)
+@app.route('/api-simulate')
+def simulate():
+    api = request.args['api']
+
+    result = get_resp_body(api)
+    his_logger.info('%s -> %s', result, result)
     return result
 
 
-@app.route('/decode', methods=['POST'])
-def decode():
-    string = request.json['text']
-    encoded = []
-    for c in string:
-        n = c
-        order = ord(c)
-        if 0x4e00 - offset <= order <= 0x9fa5 - offset:
-            n = chr(order + offset)
-        encoded.append(n)
-        logger.debug('%s(%d) to %s(%d)', c, ord(c), n, ord(n))
-    result = ''.join(encoded)
-    his_logger.info('%s -> %s', string, result)
-    return result
+def get_resp_body(api):
+    resp_body = os.path.join(resp_body_dir, '%s.json' % api)
+    with open(resp_body, 'r') as resp_body_json:
+        return json.load(resp_body_json)
 
 
 if __name__ == '__main__':
+    resp_body_dir = (len(sys.argv) == 2 and sys.argv[1] or "/www/resp_body")
     app.run(debug=True, host="0.0.0.0")

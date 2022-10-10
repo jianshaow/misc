c426619ae6192e7392be34e86f4335a399ac22ea
Jianshao <787903+jianshaow@users.noreply.github.com>
2022-10-10 18:15:32 +0800

    update to 0.0.2

--- flask/api-simulator/README.md
+++ flask/api-simulator/README.md
@@ -1,11 +1,11 @@
 
 ~~~ shell
-docker build -t jianshao/api-simulator:0.0.1 .
-docker push jianshao/api-simulator:0.0.1
+docker build -t jianshao/api-simulator:0.0.2 .
+docker push jianshao/api-simulator:0.0.2
 ~~~
 
 ~~~ shell
-docker run -d --name api-simulator --rm -p 5000:5000 -v $PWD/resp-body:/data/resp-body jianshao/api-simulator:0.0.1
+docker run -d --name api-simulator --rm -p 5000:5000 -v $PWD/resp-body:/data/resp-body jianshao/api-simulator:0.0.2
 ~~~
 
 ~~~ shell
--- flask/api-simulator/www/app.py
+++ flask/api-simulator/www/app.py
@@ -3,7 +3,7 @@ import logging
 import os
 import sys
 
-from flask import Flask, request
+from flask import Flask, make_response, render_template, request
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -11,23 +11,21 @@ logger.setLevel(logging.DEBUG)
 
 app = Flask(__name__)
 
-resp_body_dir = '/data/resp-body'
-
 
 @app.route('/mock-api/<api>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def mock(api):
-    body = get_resp_body(api, request.method)
+    body = get_resp_body(api, request)
     logger.info('request body -> %s', request.get_data(as_text=True))
     logger.info('return body -> %s', body)
-    return body
+    response = make_response(body)
+    response.content_type = 'application/json'
+    return response
 
 
-def get_resp_body(api, method):
-    resp_body = os.path.join(resp_body_dir, '{}.{}.json'.format(api, method))
-    with open(resp_body, 'r') as resp_body_json:
-        return json.load(resp_body_json)
+def get_resp_body(api, request):
+    resp_body_templ = '{}.{}.json'.format(api, request.method)
+    return render_template(resp_body_templ, args=request.args)
 
 
 if __name__ == '__main__':
-    resp_body_dir = (len(sys.argv) == 2 and sys.argv[1] or resp_body_dir)
     app.run(debug=True, host="0.0.0.0")

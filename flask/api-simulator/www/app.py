cbb79ea423f10f005b4a974d8836e7047ba0771d
Jianshao <787903+jianshaow@users.noreply.github.com>
2022-10-12 16:41:45 +0800

    update

--- flask/api-simulator/www/app.py
+++ flask/api-simulator/www/app.py
@@ -1,6 +1,7 @@
 import logging
 
 from flask import Flask, make_response, render_template, request
+from jinja2.exceptions import TemplateNotFound
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -11,17 +12,23 @@ app = Flask(__name__)
 
 @app.route('/mock-api/<api>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def mock(api):
-    body = get_resp_body(api, request)
+    body, status = get_resp_body(api, request)
     logger.info('request body -> %s', request.get_data(as_text=True))
     logger.info('return body -> %s', body)
-    response = make_response(body)
+    response = make_response(body, status)
     response.content_type = 'application/json'
     return response
 
 
 def get_resp_body(api, request):
-    resp_body_templ = '{}.{}.json'.format(api, request.method)
-    return render_template(resp_body_templ, request=request)
+    status = 200
+    resp_body_tmpl = '{}.{}.json'.format(api, request.method)
+    try:
+        resp_body = render_template(resp_body_tmpl, request=request)
+    except TemplateNotFound:
+        resp_body = ""
+        status = 404
+    return resp_body, status
 
 
 if __name__ == '__main__':

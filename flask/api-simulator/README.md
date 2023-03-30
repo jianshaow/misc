1d0fd039b4858524f098ee0ae667b99b080d1ba4
Jianshao <787903+jianshaow@users.noreply.github.com>
2023-03-30 18:53:33 +0800

    support api path group

--- flask/api-simulator/README.md
+++ flask/api-simulator/README.md
@@ -1,6 +1,6 @@
 
 ~~~ shell
-export image_ver=0.0.4
+export image_ver=0.0.5
 docker build -t jianshao/api-simulator:$image_ver .
 docker push jianshao/api-simulator:$image_ver
 
@@ -11,10 +11,10 @@ curl http://localhost:5000/mock-api/test -H 'Accept: application/xml'
 
 docker stop api-simulator
 docker run -d --name api-simulator --rm -p 5000:5000 -v $PWD/resp-body:/www/templates jianshao/api-simulator:$image_ver
-curl http://localhost:5000/mock-api/test
+curl http://localhost:5000/test-group/test
 
-kubectl -nsimulator delete cm resp-body
-kubectl -nsimulator create cm resp-body --from-file=resp-body/
+kubectl -nsimulator delete cm root-resp-body
+kubectl -nsimulator create cm root-resp-body --from-file=resp-body/
 
 kubectl apply -f manifests/deploy.yaml
 ~~~
--- flask/api-simulator/www/app.py
+++ flask/api-simulator/www/app.py
@@ -15,10 +15,18 @@ suffix_mapping = {'application/json': 'json',
                   'application/xml': 'xml', 'text/plain': 'txt', 'text/html': 'html'}
 
 
-@app.route('/mock-api/<api>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def mock(api):
-    body, content_type, status = get_resp_body(api, request)
-    request.accept_mimetypes
+@app.route('/<api>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def serve(api):
+    return mock(None, api)
+
+
+@app.route('/<group>/<api>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def serve_group(group, api):
+    return mock(group, api)
+
+
+def mock(group, api):
+    body, content_type, status = get_resp_body(group, api, request)
     logger.info('request body:\n%s', request.get_data(as_text=True))
     logger.info('return body:\n%s', body)
     response = make_response(body, status)
@@ -26,7 +34,9 @@ def mock(api):
     return response
 
 
-def get_resp_body(api, request, context={}):
+def get_resp_body(group, api, request, context={}):
+    context['group'] = group
+    context['api'] = api
     context['external_host'] = os.getenv('EXTERNAL_HOST', request.host)
     context['external_scheme'] = os.getenv('EXTERNAL_SCHEME', request.scheme)
 
@@ -34,16 +44,24 @@ def get_resp_body(api, request, context={}):
     accept_header = request.accept_mimetypes.to_header()
     content_type = accept_header
     suffix = suffix_mapping.get(accept_header, 'json')
-    resp_body_tmpl = '{}.{}.{}'.format(api, request.method, suffix)
+    resp_body_tmpl = '{}/{}.{}.{}'.format(
+        get_template_path(group), api, request.method, suffix)
     try:
         resp_body = render_template(
             resp_body_tmpl, request=request, context=context)
     except TemplateNotFound:
-        resp_body = '{{"error": "no api mockup \'{}\' on \'{}\' with type \'{}\'"}}\n'.format(
-            api, request.method, accept_header)
+        resp_body = '{{"error": "no api mockup \'{}\' in group \'{}\' on \'{}\' with type \'{}\'"}}\n'.format(
+            api, group, request.method, accept_header)
         content_type = 'application/json'
         status = 404
     return resp_body, content_type, status
+
+
+def get_template_path(group):
+    if group:
+        return group
+    else:
+        return '__root'
 
 
 if __name__ == '__main__':
